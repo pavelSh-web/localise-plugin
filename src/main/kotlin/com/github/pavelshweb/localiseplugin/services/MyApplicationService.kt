13ad5c7a69dab0222a06e8a95955c3a63738f503
GitHub Action <action@github.com>
2022-03-10 21:57:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/pavelshweb/localiseplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pavelshweb/localiseplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pavelshweb.localiseplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pavelshweb.localiseplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/pavelshweb/localiseplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pavelshweb/localiseplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pavelshweb.localiseplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pavelshweb.localiseplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
